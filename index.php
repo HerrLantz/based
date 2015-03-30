d4769deb7a19b5f18e6d3d70113809f6d200d166
HerrLantz <patriclantz@hotmail.com>
2015-03-30 02:02:59 +0200

    La till lite skit

--- index.php
+++ index.php
@@ -1,17 +1,50 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 	<html>
 	<head>
+		<link href="styles/style.css" rel="stylesheet">
 		<title>Stunna!</title>
 	</head>
 	<body>
-		<?php
-			$wadidis = "Korv";
-			for ($i = 0; $i < 1000; $i++) {
-				echo $wadidis . " ";
-				if ($i%10 == 0) {
-					echo "<br>";
-				}
-			}
-		?>
+		<div class="header">
+			<h1>Köp korv nu!</h1>
+		</div>
+		<div class="sign-in">
+			<div id="as-buyer">
+				<table>
+					<tr>
+						<td>
+							<h3>Logga in som korvköpare</h3>
+						</td>
+					</tr>
+					<tr>
+						<input type="text" name="namn" value="<?php echo $name;?>">
+					</tr>
+					<tr>
+						
+					</tr>
+					<tr>
+						
+					</tr>
+				</table>
+			</div>
+			<div id="as-seller">
+				<table>
+					<tr>
+						<td>
+							<h3>Logga in som korvsäjare</h3>
+						</td>
+					</tr>
+					<tr>
+						
+					</tr>
+					<tr>
+						
+					</tr>
+					<tr>
+						
+					</tr>
+				</table>
+			</div>
+		</div>
 	</body>
 </html>

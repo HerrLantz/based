328923ba184187c58a66ffaaea6c016e152d7dcc
Nezz94 <nedo@kth.se>
2015-04-15 22:10:58 +0200

    logga in som förare och se sina korvtrakt
    u fil me?

---
+++ driver_contracts.php
@@ -0,0 +1,93 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<link href="styles/style.css" rel="stylesheet">
+		<title>Stunna!</title>
+	</head>
+	<body>
+		<div class="header">
+			<h1>Mina korvtrakt</h1>
+		</div>
+		<div class="container">
+			<h2>Korvtrakt du ska transportera</h2>
+			<?php
+				$servername = "localhost";
+				$username = "root";
+				$password = "";
+				$dbname = "labb2";
+				// Create connection
+				$conn = mysqli_connect($servername, $username, $password, $dbname);
+				// Check connection
+				if (!$conn) {
+    				die("Connection failed: " . mysqli_connect_error());
+				}
+				echo "<table style='width:100%'>
+								<tr>
+									<th align='left'>ID</th>
+									<th align='left'>Status</th>
+								</tr>";
+				$sql = "SELECT * FROM dropsoff
+						WHERE driverID =" . $_POST["driverID"] . "";
+				$result = mysqli_query($conn, $sql);
+				if(!$result) {
+					printf("Ett fel uppstod. Se till att du fyllt i rätt förarnummer.");
+					exit();
+				}
+				if(mysqli_num_rows($result) > 0) {
+					while($row = mysqli_fetch_assoc($result)) {
+						echo "<tr>
+								<td>" . $row["packageID"] ."</td>
+								<td>Levererat</td>
+								<td>
+									<form action='' method='post'>";
+									//Använd om packet nummer måste skickas vidare till nästa sida.
+									//<input type='hidden' name='korvtraktID' value=" . $row["packageID"] . ">"
+						echo	"</td>
+							</tr>";
+					}
+				}
+				$sql = "SELECT * FROM picksup
+						WHERE driverID =" . $_POST["driverID"] . "";
+				$result = mysqli_query($conn, $sql);
+				if(!$result) {
+					printf("Ett fel uppstod. Se till att du fyllt i rätt förarnummer.");
+					exit();
+				}
+				if(mysqli_num_rows($result) > 0) {
+					while($row = mysqli_fetch_assoc($result)) {
+						echo "<tr>
+								<td>" . $row["packageID"] ."</td>
+								<td>Hämtat</td>
+								<td>
+									<form action='' method='post'>";
+									//Använd om packet nummer måste skickas vidare till nästa sida.
+									//<input type='hidden' name='korvtraktID' value=" . $row["contractID"] . ">
+						echo		"<input type='submit' value='Uppdatera status'>
+								</td>
+							</tr>";
+					}
+				}
+				$sql = "SELECT * FROM takes 
+						WHERE driverID =" . $_POST["driverID"] . "";
+				$result = mysqli_query($conn, $sql);
+				if(!$result) {
+					printf("Ett fel uppstod. Se till att du fyllt i rätt förarnummer.");
+					exit();
+				}
+				if(mysqli_num_rows($result) > 0) {
+					while($row = mysqli_fetch_assoc($result)) {
+						echo "<tr>
+								<td>" . $row["contractID"] ."</td>
+								<td>Ej Hämtat</td>
+								<td>
+									<form action='' method='post'>
+									<input type='hidden' name='korvtraktID' value=" . $row["contractID"] . ">
+									<input type='submit' value='Uppdatera status'>
+								</td>
+							</tr>";
+					}
+				}
+			?>
+		</div>
+	</body>
+</html>
---
+++ driver_login.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<link href="styles/style.css" rel="stylesheet">
+		<title>Stunna!</title>
+	</head>
+	<body>
+		<div class="header">
+			<h1>Logga in</h1>
+		</div>
+		<div class="container">
+				<form action="driver_contracts.php" method="post">
+					<p>Ditt förarnummer</p>
+					<input type="number" name="driverID" required>
+					<br>
+					<div class="continue_button">
+						<input type="submit" value="Logga in">
+				</form>
+			</div>
+		</div>
+	</body>
+</html>

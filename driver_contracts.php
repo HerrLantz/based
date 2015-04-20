4ab888085ec8a48b1b8bc6bf768af9f17130a3ad
Nezz94 <nedo@kth.se>
2015-04-20 18:15:26 +0200

    Gjort om driver_contracts
    Nu ser man bara alla paket. Blir enklare så här

--- driver_contracts.php
+++ driver_contracts.php
@@ -21,92 +21,97 @@
 				if (!$conn) {
     				die("Connection failed: " . mysqli_connect_error());
 				}
-				$dropsql = "SELECT * FROM dropsoff
+				$dropsql = "SELECT dropsoff.packageID, contractID, time 
+							FROM dropsoff JOIN package
+								ON dropsoff.packageID = package.packageID
 							WHERE driverID =" . $_POST["driverID"] . "";
 				$dropresult = mysqli_query($conn, $dropsql);
 				if(!$dropresult) {
 					printf("Ett fel uppstod. Se till att du fyllt i rätt förarnummer.");
 					exit();
 				}
-				$picksql = "SELECT packageID, time FROM picksup
+				$picksql = "SELECT picksup.packageID, package.contractID, time 
+							FROM picksup JOIN package
+								ON picksup.packageID = package.packageID
 							WHERE driverID =" . $_POST["driverID"] . "
-							AND (packageID) NOT IN 
+							AND (picksup.packageID) NOT IN 
 							(SELECT packageID FROM dropsoff)";
 				$pickresult = mysqli_query($conn, $picksql);
 				if(!$pickresult) {
 					printf("Ett fel uppstod. Se till att du fyllt i rätt förarnummer.");
 					exit();
 				}
-				if(mysqli_num_rows($pickresult) > 0 || mysqli_num_rows($dropresult) > 0) {
+				$takensql = "SELECT packageID, takes.contractID, takes.time FROM
+							takes JOIN package
+								ON takes.contractID = package.contractID
+							WHERE driverID = " . $_POST["driverID"] . "
+							AND packageID NOT IN
+							(SELECT packageID FROM picksup)";
+				$takenresult = mysqli_query($conn, $takensql);
+				if(!$takenresult) {
+					printf("Ett fel uppstod. Se till att du fyllt i rätt förarnummer.");
+					exit();
+				}
+				if(mysqli_num_rows($pickresult) > 0 || mysqli_num_rows($dropresult) > 0 || mysqli_num_rows($takenresult) > 0) {
 					echo "<table style='width:100%'>
 								<tr>
 									<th align='left'>Paketnummer</th>
 									<th align='left'>Status</th>
 									<th align='left'>Senast uppdaterat</th>
+									<th align='left'>Del av korvtrakt</th>
+									<th align='left'></th>
 								</tr>";
 				} else {
 					echo "Inga korvpaket håller på transporteras.";
 				}
+				// Print out all "hämtade" packages
 				if(mysqli_num_rows($pickresult) > 0) {
 					while($row = mysqli_fetch_assoc($pickresult)) {
 						echo "<tr>
 								<td>" . $row["packageID"] ."</td>
 								<td>Hämtat</td>
 								<td>" . $row["time"] . "</td>
+								<td>" . $row["contractID"] . "</td>
 								<td>
 									<form action='driver_packageupdate.php' method='post'>
 										<input type='hidden' name='packageID' value=" . $row["packageID"] . ">
 										<input type='hidden' name='driverID' value=" . $_POST["driverID"] . ">
-										<input type='submit' value='Uppdatera status'>	
+										<input type='submit' value='Uppdatera status'>
+									</form>	
 								</td>
 							</tr>";
 					}
 				}
+				// Print out taken packages (not hämtade)
+				if(mysqli_num_rows($takenresult) > 0) {
+					while($row = mysqli_fetch_assoc($takenresult)) {
+						echo "<tr>
+								<td>" . $row["packageID"] ."</td>
+								<td>Ej hämtat</td>
+								<td>" . $row["time"] . "</td>
+								<td>" . $row["contractID"] . "</td>
+								<td>
+									<form action='driver_packageupdate.php' method='post'>
+										<input type='hidden' name='packageID' value=" . $row["packageID"] . ">
+										<input type='hidden' name='driverID' value=" . $_POST["driverID"] . ">
+										<input type='submit' value='Uppdatera status'>
+									</form>	
+								</td>
+							</tr>";
+					}
+				}
+				// Print out delivered packages. 
 				if(mysqli_num_rows($dropresult) > 0) {
 					while($row = mysqli_fetch_assoc($dropresult)) {
 						echo "<tr>
 								<td>" . $row["packageID"] ."</td>
 								<td>Levererat</td>
 								<td>" . $row["time"] . "</td>
+								<td>" . $row["contractID"] . "</td>
 							</tr>";
 					}
-					echo "</table><br>";
-				}	
-			?>
-		</div>
-		<div class = "contracts_container">
-			<h2>Korvtrakt du ska transportera</h2>
-			<?php
-				$sql = "SELECT * FROM takes 
-						WHERE driverID =" . $_POST["driverID"] . "";
-				$result = mysqli_query($conn, $sql);
-				if(!$result) {
-					printf("Ett fel uppstod. Se till att du fyllt i rätt förarnummer.");
-					exit();
 				}
-				if(mysqli_num_rows($result) > 0) {
-					echo "<table style='width:100%'>
-								<tr>
-									<th align='left'>Korvtraktnummer</th>
-									<th align='left'>Status</th>
-								</tr>";
-					while($row = mysqli_fetch_assoc($result)) {
-						echo "<tr>
-								<td>" . $row["contractID"] ."</td>
-								<td>Ej påbörjat leverans</td>
-								<td>
-									<form action='driver_contractupdate.php' method='post'>
-									<input type='hidden' name='korvtraktID' value=" . $row["contractID"] . ">
-									<input type='hidden' name='driverID' value=" . $_POST["driverID"] . ">
-									<input type='submit' value='Uppdatera status'>
-									</form>
-								</td>
-							</tr>";
-					}
-					echo "</table>";
-				} else {
-					echo "Inga korvtrakt tillgängliga för upphämtning.";
-				}
+				echo "</table>";
 				mysqli_close($conn);
 			?>
 		</div>
